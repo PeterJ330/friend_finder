12dd9bf1ae1ba279514719d73d108f7eba6bb16a
peterj330 <peter.a.jarrard@gmail.com>
2018-09-18 23:55:51 -0500

    refactor

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,25 @@
 
 var friendsData = [
 {
-    "name": "Peter" ,
+    "name": "Peter Jarrard" ,
     "photo": "photo" ,
-    "scores":[] ,
+    "scores":[3, 5, 2, 4, 6, 1, 4, 5, 4, 1] ,
+  }, {
+    "name": "Tony Romo" ,
+    "photo": "photo" ,
+    "scores":[4, 2, 5, 3, 1, 5, 2, 3, 1, 5] ,
+  }, {
+    "name": "Benjamin Franklin" ,
+    "photo": "photo" ,
+    "scores":[5, 4, 1, 1, 5, 4, 1, 5, 2, 3] ,
+  }, {
+    "name": "Alfred Hitchcock" ,
+    "photo": "photo" ,
+    "scores":[3, 1, 3, 3, 5, 5, 3, 2, 2, 1] ,
+  }, {
+    "name": "Keanu Reeves" ,
+    "photo": "photo" ,
+    "scores":[2, 1, 2, 3, 3, 1, 4, 1, 3, 1] ,
   }
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,7 +3,7 @@ var friends = require("../data/friends");
 
 
 module.exports = function(app) {
-// All of objectArray
+// A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
 app.get("/api/friends", function(req, res) {
     res.json(friends);
 });
@@ -11,25 +11,24 @@ app.get("/api/friends", function(req, res) {
 // displays a single object in friendsData, or returns false
 app.get("/api/friends/:friend", function(req, res) {
     var input = req.params.friend ;
-    console.log(input);
 
     for (var i = 0; i < friends.length; i++) {
-        if (input === friends[i].name) {
+        if (input == friends[i].name.toLowerCase()) {
             return res.json(friends[i]);
         }
-    }
+    };
     return res.json(false);
+    
 });
-
-
-// A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
 
 
 // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic. 
-app.post("/api/add-survey", function(req, res) {
+app.post("/api/friends", function(req, res) {
     friends.push(req.body);
     res.json(true);
 });
+
+
 
 
 };
