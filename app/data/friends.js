de06e5f70b7adfd424240b8d86b6138f419c0e0c
peterj330 <peter.a.jarrard@gmail.com>
2018-10-20 16:20:13 -0500

    ..

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,24 +1,24 @@
 
 var friendsData = [
 {
-    "name": "Peter Jarrard" ,
-    "photo": "photo" ,
+    "name": "Dwayne 'The Rock' Johnson" ,
+    "photo": "https://cdn.gobankingrates.com/wp-content/uploads/2018/07/Dwayne-The-Rock-Johnson-shutterstock_105249263.jpg" ,
     "scores":[3, 5, 2, 4, 6, 1, 4, 5, 4, 1] ,
   }, {
     "name": "Tony Romo" ,
-    "photo": "photo" ,
+    "photo": "https://media.golfdigest.com/photos/58e3c330418ffa3c84413bcd/master/w_780,c_limit/GettyImages-461028712.jpg" ,
     "scores":[4, 2, 5, 3, 1, 5, 2, 3, 1, 5] ,
   }, {
     "name": "Benjamin Franklin" ,
-    "photo": "photo" ,
+    "photo": "https://thumbs-prod.si-cdn.com/S688lYwrKJhKQ4vFdBrOFvq6TEw=/800x600/filters:no_upscale()/https://public-media.smithsonianmag.com/filer/20110520090044benjamin-franklin-520.jpg" ,
     "scores":[5, 4, 1, 1, 5, 4, 1, 5, 2, 3] ,
   }, {
     "name": "Alfred Hitchcock" ,
-    "photo": "photo" ,
+    "photo": "https://nerdist.com/wp-content/uploads/2015/05/Directors-Cuts-Hitchcock-060115.jpg" ,
     "scores":[3, 1, 3, 3, 5, 5, 3, 2, 2, 1] ,
   }, {
     "name": "Keanu Reeves" ,
-    "photo": "photo" ,
+    "photo": "https://i.kinja-img.com/gawker-media/image/upload/glcjptyyamd5l1pxd1z9.jpg" ,
     "scores":[2, 1, 2, 3, 3, 1, 4, 1, 3, 1] ,
   }
 ];
